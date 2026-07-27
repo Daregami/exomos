44f2d10cc1e01fdad4d9c185c2870c159f282aca
Daregami <burdeynyy-2004@mail.ru>
2026-07-27 08:35:51 +0300

    fix: lba instead of sector

--- boot/boot.asm
+++ boot/boot.asm
@@ -20,21 +20,19 @@ _start:
     
     mov si,msg_load
     call print_str
-
-    ; Чтение секторов загрузчика
-    mov ah,0x02
-    mov al,128 ; количество секторов - 64кб на загрузчик
-    mov cx,2 ; ch=0 - номер цилиндра; cl=2 - со второго сектора
-    mov dh,0 ; номер головки
+    
+    ; Копирование с помощью lba
+    mov ah,0x42
     mov dl,[number_disk]
-    mov bx,0x7E00 ; пишем сразу после MBR
+    mov si,disk_dap
     int 0x13
 
     jc disk_error
 
     mov si,msg_ok
     call print_str
-
+    
+    mov dl,[number_disk]
     jmp 0x7E00 ; если ошибок не было, прыгаем в загрузчик
 
 %include "print.asm"
@@ -52,6 +50,16 @@ msg_load db 'Loading bootloader... ',0
 msg_ok db 'OK',0x0D,0x0A,0
 error_disk db 'Disk error...',0
 number_disk db 0
+
+align 4
+disk_dap:
+    db 0x10 ; размер пакета
+    db 0x00
+    dw 128 ; количество секторов под загрузчик
+    dw 0x7E00 ; смещение bx
+    dw 0x0000 ; сегмент es
+.lba_sector:
+    dq 1 ; начальный сектор загрузчика
 
 times 510-($-$$) db 0
 dw 0xAA55
--- boot/loader.asm
+++ boot/loader.asm
@@ -5,6 +5,10 @@ _load_start:
     mov si,msg_kload
     call print_str
 
+copy_kernel:
+    mov si,msg_ok
+    call print_str
+
 stop_system:
     cli
     hlt
@@ -14,5 +18,12 @@ stop_system:
 
 msg_kload db 'Load kernel... ',0
 msg_ok db 'OK',0x0D,0x0A,0
+
+bios_gdt:
+    dq 0 ; нулевой дескриптор
+    dq 0 ; настраивается BIOS
+
+    ; Источник данных данных из буфера
+
 
 times 65536-($-$$) db 0
