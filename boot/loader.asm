8b0a7c6305b992ff010d5faac0fde908a77527ce
Daregami <burdeynyy-2004@mail.ru>
2026-07-28 00:31:30 +0300

    Kernel stub loader completed

--- boot/loader.asm
+++ boot/loader.asm
@@ -6,7 +6,11 @@ _load_start:
     call print_str
 
 copy_kernel:
-    mov bp,64 ; количество итераций для загрузки ядра - 2мб / 32кб = 64
+    mov bp,32 ; количество итераций для загрузки ядра - 2мб / 64кб = 32
+
+.loop:
+    xor ax,ax
+    mov ds,ax
 
     mov ah,0x42
     mov si,disk_dap
@@ -14,8 +18,29 @@ copy_kernel:
 
     jc disk_error
 
+    mov cx, 0x8000 ; счетчик пересылаемых 16-битовых слов
+    mov si, bios_gdt
+    xor ax, ax
+    mov es, ax
+    mov ax, 0x8700 ; функция переноса блока
+    int 0x15
+
+    jc bios_error
+
+    add dword [disk_dap + 8],128 ; 128 * 512 = 64кб
+    add byte [bios_gdt + 28], 0x0001 ; 24 байт до дескриптора приемника
+                                     ; + 4 увеличиваем базовый адрес на 64кб
+
+    dec bp
+    jnz .loop ; считали нужное количество блоков - выходим
+
     mov si,msg_ok
     call print_str
+
+    ;mov ax,0x1000
+    ;mov ds,ax
+    ;mov si,0x7E00
+    ;call print_str
 
 stop_system:
     cli
@@ -32,18 +57,27 @@ disk_error:
     hlt
     jmp .halt
 
+bios_error:
+    mov si,error_bios
+    call print_str
+.halt:
+    cli
+    hlt
+    jmp .halt
+
 msg_kload db 'Load kernel... ',0
 error_disk db 'Disk error... ',0
+error_bios db 'Bios error... ',0
 msg_ok db 'OK',0x0D,0x0A,0
 
 align 4
 disk_dap:
     db 0x10
     db 0
-    dw 64 ; читаем 64 сектора за раз в буфер
+    dw 128 ; читаем 128 сектора за раз в буфер
     ; Адрес 0x00017E00 (буфер)
-    dw 0x7E00       ; Смещение буфера
-    dw 0x1000       ; Сегмент буфера
+    dw 0x0000 ; Смещение буфера
+    dw 0x2000 ; Сегмент буфера
 dap_lba:
     dq 129
 
@@ -54,8 +88,8 @@ bios_gdt:
 
     ; Источник данных данных из буфера
     dw 0xFFFF ; предел перемещаемого куска
-    dw 0x7E00 ; базовый адрес буфера
-    dw 0x0001 ; средний базовый адрес
+    dw 0x0000 ; базовый адрес буфера
+    db 0x02 ; средний базовый адрес
     db 0x93 ; права доступа чтение/запись
             ; находятся в памяти, данные, в Ring0
     db 0x00
@@ -64,7 +98,7 @@ bios_gdt:
     ; Приемник данных данных из буфера
     dw 0xFFFF
     dw 0x0000 ; базовый адрес расположения ядра
-    base_addr dw 0x0010 ; средний базовый адрес
+    db 0x10 ; средний базовый адрес
     db 0x93 ; права доступа чтение/запись
             ; находятся в памяти, данные, в Ring0
     db 0x00
--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ nasm -f bin boot/boot.asm -i boot/ -o build/boot.bin
 nasm -f bin boot/loader.asm -i boot/ -o build/loader.bin
 
 dd if=/dev/zero of=build/kernel bs=2M count=500
+printf "ELF123" | dd of=build/kernel bs=1 count=6 conv=notrunc
 
 cat build/boot.bin build/loader.bin build/kernel > build/exomos.bin
 
